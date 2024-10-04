a5490850192f8edb44c40f1786730b74b5e3eb66
RyojiDev <ryojikitanodev@gmail.com>
2024-10-04 18:42:06 -0300

    --author=Ryojidev ryojikitanodev@gmail.com
    add styles and components

--- ecommerce/essencial/frontend/src/components/produto/ProdutoItem.tsx
+++ ecommerce/essencial/frontend/src/components/produto/ProdutoItem.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import { Produto } from "@/core"
-
+import Image from "next/image"
 export interface ProdutoItemProps {
     produto: Produto
 }
@@ -8,5 +8,13 @@ export interface ProdutoItemProps {
 export default function ProdutoItem(props: ProdutoItemProps) {
     const { produto } = props
     return <Link href={`/produto/${produto.id}`}
-    className="flex flex-col">{produto.nome}</Link>
+    className="flex flex-col bg-violet-dark border boder-white/10 rounded-xl relative max-w-[350px]">
+        <div className="h-48 w-full relative">
+          <Image
+          src={produto.imagem}
+          fill
+          alt='Imagem do produto'
+          />  
+        </div>
+        {produto.nome}</Link>
 }
